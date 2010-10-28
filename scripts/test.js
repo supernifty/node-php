ea95ff9b9018fbf96233994df70bc1733496b804
Peter Georgeson <peter@supernifty.com.au>
2010-10-29 00:45:19 +1100

    fix encode_session for integer values

--- scripts/test.js
+++ scripts/test.js
@@ -5,6 +5,7 @@ assert.equal( php.md5crypt('rasmuslerdorf', '$1$rasmusle$'), '$1$rasmusle$rISCgZ
 
 // key|s:length:"data";
 assert.equal( php.encode_session( { 'n': '123', 's': 'abc' } ), 'n|s:3:"123";s|s:3:"abc";' );
+assert.equal( php.encode_session( { 'n': 123, 's': 'abc' } ), 'n|s:3:"123";s|s:3:"abc";' );
 assert.equal( JSON.stringify( php.decode_session( 'n|s:3:"123";s2|s:3:"abc";' ) ), JSON.stringify( {"n":"123","s2":"abc"} ) );
 assert.equal( php.generate_session().length, 26 );
 assert.equal( typeof php.generate_session(), "string" );
